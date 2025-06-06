052f372f224ed8b6db912826d2591100dbf23828
Furglitch <samsonger@proton.me>
2025-06-05 22:49:50 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ case "$1" in
         printf "Starting Docker containers...\n"
         for dir in docker-compose/*; do
             if [ -d "$dir" ]; then
-                printf "Starting containers from compose: $dir"
+                printf "Starting containers from compose: $dir \n"
                 docker-compose -f "$dir/docker-compose.yaml" up -d
             fi
         done
@@ -19,7 +19,7 @@ case "$1" in
         printf "Restarting Docker containers...\n"
         for dir in docker-compose/*; do
             if [ -d "$dir" ]; then
-                printf "Restarting containers from compose: $dir"
+                printf "Restarting containers from compose: $dir \n"
                 docker-compose -f "$dir/docker-compose.yaml" restart
             fi
         done
@@ -28,7 +28,7 @@ case "$1" in
         printf "Stopping Docker containers...\n"
         for dir in docker-compose/*; do
             if [ -d "$dir" ]; then
-                printf "Stopping containers from compose: $dir"
+                printf "Stopping containers from compose: $dir \n"
                 docker-compose -f "$dir/docker-compose.yaml" down
             fi
         done
